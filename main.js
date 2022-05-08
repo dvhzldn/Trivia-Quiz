75222e8592eb0a43d30c9b1740109cbab257178d
dhazeldean <davehazeldean@gmail.com>
2022-05-08 13:38:23 +0100

    Fixed hover colouring of buttons. Tidied up code ordering in main.js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 // API address: https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
 // API docs address: https://opentdb.com/api_config.php
 // -----------
-// Declare variables
-
-//Define variables for HTML elements
+// 
+//
+//Variables for HTML elements
 let box1 = document.getElementById("Box1")
 let box2 = document.getElementById("Box2")
 let box3 = document.getElementById("Box3")
 let box4 = document.getElementById("Box4")
 let questionText = document.getElementById("question");
 let questionNumber = document.getElementById("questionnumber");
-
 let nextQuestionButton = document.querySelector("#Next");
 let correctAnswerText = document.querySelector("#correctText");
 let playerAnswerText = document.querySelector("#playerAnswer");
@@ -20,12 +19,13 @@ let scoreText = document.getElementById("score");
 let currentQuestion = 1;
 let score = 0;
 
+//Function to get API token
 async function getToken() {
     let tokenresponse = await fetch("https://opentdb.com/api_token.php?command=request");
     dataToken = await tokenresponse.json();
 }
 
-// Function to get questions from API
+// Function to get quiz data from API
 async function getData() {
     category = document.querySelector("#category").value;
     difficulty = document.querySelector("#difficulty").value;
@@ -37,6 +37,35 @@ async function getData() {
     ;
 }
 
+//Run initial start function that loads starting page
+start();
+
+//Function to show settings and hide quiz when user initially lands on page
+function start() {
+    document.getElementById("settings").style.display = "block";
+    document.getElementById("restart").style.display = "none";
+    document.getElementById("quiz").style.display = "none";
+    document.getElementById("score").style.display = "none";
+    getToken();
+}
+
+//Function to start a round of the game
+function startGame(){
+    category = document.querySelector("#category").value;
+    difficulty = document.querySelector("#difficulty").value;
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("score").style.display = "none";
+    document.getElementById("quiz").style.display = "none";
+    currentQuestion = 1;
+    score = 0;
+    hideResult();
+    getData();
+    document.getElementById("quiz").style.display = "block";
+    document.getElementById("restart").style.display = "block";
+    showAnswerBoxes();
+}
+
+//Function to populate question and assign answers
 function getQuestions() {    
     question = data.results[(currentQuestion - 1)].question
     correctAnswer = data.results[(currentQuestion - 1)].correct_answer;
@@ -47,7 +76,7 @@ function getQuestions() {
     questionText.innerHTML = question;
     randomiser();}
 
-// Function to randomise which box contains the correct answer
+// Function to randomise which box contains the correct answer and populate answers
 function randomiser() {
     let randomNumber = Math.random();
 
@@ -76,14 +105,6 @@ function randomiser() {
         box4.innerHTML = correctAnswer;}
     }
 
-//Function to show the correct answer
-function showAnswer () {
-    correctAnswerText.innerHTML = `The answer is ${correctAnswer}`;
-    correctAnswerText.style.display = "block";
-    nextQuestionButton.style.display = "block";
-    playerAnswerText.style.display = "block";
-}
-
 //Function to show the answer boxes at the start of each round
 function showAnswerBoxes () {
     box1.style.display = "block";
@@ -92,7 +113,7 @@ function showAnswerBoxes () {
     box4.style.display = "block";
 }
 
-//Function to hide the answer boxes when question is submitted
+//Function to hide the answer boxes
 function hideAnswerBoxes () {
     box1.style.display = "none";
     box2.style.display = "none";
@@ -111,11 +132,6 @@ function submitAnswer(){
         playerAnswerText.innerHTML = `Wrong. You chose ${playerAnswer}`;
     }
 }
-//Function to hide the result of each round
-function hideResult () {
-    nextQuestionButton.style.display = "none";
-    correctAnswerText.style.display = "none";
-    playerAnswerText.style.display = "none";}
 
 //Function to advance to next question
 function nextQuestion(){
@@ -129,39 +145,10 @@ function nextQuestion(){
     };
 }
 
-//Listening for clicks on Submit and next question buttons
-nextQuestionButton.addEventListener("click", nextQuestion);
-
-//Listening for clicks on boxes
-//Functions to assign playerAnswer based on button that's clicked
-box1.addEventListener("click", function onClick() {
-    playerAnswer = box1.innerHTML;
-    submitAnswer();
-});
-
-box2.addEventListener("click", function onClick() {
-    playerAnswer = box2.innerHTML;
-    submitAnswer();
-});
-box3.addEventListener("click", function onClick() {
-    playerAnswer = box3.innerHTML;
-    submitAnswer();
-});
-box4.addEventListener("click", function onClick() {
-    playerAnswer = box4.innerHTML;
-    submitAnswer();
-});
-
-function restartGame(){
-    currentQuestion = 1;
-    score = 0;
-    hideResult();
-    getToken();
-    getData(); 
-}
+//Function to show the score at the end of the game and ask if another round is to be played
 function endGame(){
     document.getElementById("quiz").style.display = "none";
-    document.getElementById("quit").style.display = "none";
+    document.getElementById("restart").style.display = "none";
     scoreText.innerText = `End of quiz.\n\nYou scored ${score} out of ${numberOfQuestions}.`;
     document.getElementById("score").style.display = "block";
     setTimeout(() => {
@@ -174,38 +161,47 @@ function endGame(){
     }, "2000")
 }
 
-//getToken();
-//hideResult();
-//getData();
-
-function startGame(){
-    category = document.querySelector("#category").value;
-    difficulty = document.querySelector("#difficulty").value;
-    document.getElementById("settings").style.display = "none";
-    document.getElementById("score").style.display = "none";
-    document.getElementById("quiz").style.display = "none";
+//Function to restart game when player plays another round
+function restartGame(){
     currentQuestion = 1;
     score = 0;
     hideResult();
-    getData();
-    document.getElementById("quiz").style.display = "block";
-    document.getElementById("quit").style.display = "block";
-    showAnswerBoxes();
-}
-
-function start() {
-    document.getElementById("settings").style.display = "block";
-    document.getElementById("quit").style.display = "none";
-    document.getElementById("quiz").style.display = "none";
-    document.getElementById("score").style.display = "none";
     getToken();
+    getData(); 
 }
 
-function closeForm() {
+//Function to show the correct answer
+function showAnswer () {
+    correctAnswerText.innerHTML = `The answer is ${correctAnswer}`;
+    correctAnswerText.style.display = "block";
+    nextQuestionButton.style.display = "block";
+    playerAnswerText.style.display = "block";
 }
 
-function quit() {
-    start();
-}
+//Function to hide the result of each round
+function hideResult () {
+    nextQuestionButton.style.display = "none";
+    correctAnswerText.style.display = "none";
+    playerAnswerText.style.display = "none";}
 
-start();
+//Listening for click on next question button
+nextQuestionButton.addEventListener("click", nextQuestion);
+
+//Listening for click on restart button
+nextQuestionButton.addEventListener("restart", start);
+
+
+//Listening for clicks on answer boxes
+//Functions to assign playerAnswer based on button that's clicked
+box1.addEventListener("click", function onClick() {
+    playerAnswer = box1.innerHTML;
+    submitAnswer();});
+box2.addEventListener("click", function onClick() {
+    playerAnswer = box2.innerHTML;
+    submitAnswer();});
+box3.addEventListener("click", function onClick() {
+    playerAnswer = box3.innerHTML;
+    submitAnswer();});
+box4.addEventListener("click", function onClick() {
+    playerAnswer = box4.innerHTML;
+    submitAnswer();});
